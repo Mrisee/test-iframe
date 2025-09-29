107f02028550fd0aa2c806688cfcd0b5e83155f0
diakudza <77148342+diakudza@users.noreply.github.com>
2025-09-29 12:12:00 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log('внутри massage');
     iframe.contentWindow.postMessage(
       {
-        type: "message",
+        type: "init",
         parentUrl: window.location.href,
         referrer: document.referrer
       },
