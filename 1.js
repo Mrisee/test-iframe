b0b78e2d2b4e48f0b1c87c5e333c5f69554e458d
diakudza <77148342+diakudza@users.noreply.github.com>
2025-09-29 11:43:33 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,12 +1,26 @@
 console.log('script');
-window.addEventListener('message', function (event) {
-    console.log('listener');
-    if (event.data.type === 'sendParentUrl') {
-        console.log('if');
-        event.source.postMessage({
-            type: 'parentUrl',
-            url: window.location.href
-        }, '*');
-    }
-});
+// window.addEventListener('message', function (event) {
+//     console.log('listener');
+//     if (event.data.type === 'sendParentUrl') {
+//         console.log('if');
+//         event.source.postMessage({
+//             type: 'parentUrl',
+//             url: window.location.href
+//         }, '*');
+//     }
+// });
+
+const iframe = document.querySelector("iframe");
+    // Когда iframe загрузился, шлём ему данные
+    iframe.addEventListener("message", () => {
+      
+      iframe.contentWindow.postMessage(
+        {
+          type: "init",
+          parentUrl: window.location.href, // URL партнёрской страницы
+          referrer: document.referrer,     // можно ещё реферер
+        },
+        "*"
+      );
+    });
      
